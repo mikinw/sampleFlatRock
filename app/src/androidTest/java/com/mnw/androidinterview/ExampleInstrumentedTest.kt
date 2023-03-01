mikinw/sample

template from android studio

app/src/androidTest/java/com/mnw/deverestinterview/ExampleInstrumentedTest.kt
@@ -1,0 +1,24 @@
+package com.mnw.deverestinterview
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.mnw.deverestinterview", appContext.packageName)
+    }
+}

app/src/main/java/com/mnw/deverestinterview/ItemDetailFragment.kt
@@ -1,0 +1,98 @@
+package com.mnw.deverestinterview
+
+import android.content.ClipData
+import android.os.Bundle
+import android.view.DragEvent
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.mnw.deverestinterview.placeholder.PlaceholderContent
+import com.mnw.deverestinterview.databinding.FragmentItemDetailBinding
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a [ItemListFragment]
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+class ItemDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    lateinit var itemDetailTextView: TextView
+    private var toolbarLayout: CollapsingToolbarLayout? = null
+
+    private var _binding: FragmentItemDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val dragListener = View.OnDragListener { v, event ->
+        if (event.action == DragEvent.ACTION_DROP) {
+            val clipDataItem: ClipData.Item = event.clipData.getItemAt(0)
+            val dragData = clipDataItem.text
+            item = PlaceholderContent.ITEM_MAP[dragData]
+            updateContent()
+        }
+        true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        toolbarLayout = binding.toolbarLayout
+        itemDetailTextView = binding.itemDetail
+
+        updateContent()
+        rootView.setOnDragListener(dragListener)
+
+        return rootView
+    }
+
+    private fun updateContent() {
+        toolbarLayout?.title = item?.content
+
+        // Show the placeholder content as text in a TextView.
+        item?.let {
+            itemDetailTextView.text = it.details
+        }
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

app/src/main/java/com/mnw/deverestinterview/ItemDetailHostActivity.kt
@@ -1,0 +1,35 @@
+package com.mnw.deverestinterview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.mnw.deverestinterview.databinding.ActivityItemDetailBinding
+
+
+
+class ItemDetailHostActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_item_detail) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_item_detail)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}

app/src/main/java/com/mnw/deverestinterview/ItemListFragment.kt
@@ -1,0 +1,190 @@
+package com.mnw.deverestinterview
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.os.Build
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.mnw.deverestinterview.placeholder.PlaceholderContent;
+import com.mnw.deverestinterview.databinding.FragmentItemListBinding
+import com.mnw.deverestinterview.databinding.ItemListContentBinding
+
+/**
+ * A Fragment representing a list of Pings. This fragment
+ * has different presentations for handset and larger screen devices. On
+ * handsets, the fragment presents a list of items, which when touched,
+ * lead to a {@link ItemDetailFragment} representing
+ * item details. On larger screens, the Navigation controller presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+
+class ItemListFragment : Fragment() {
+
+    /**
+     * Method to intercept global key events in the
+     * item list fragment to trigger keyboard shortcuts
+     * Currently provides a toast when Ctrl + Z and Ctrl + F
+     * are triggered
+     */
+    private val unhandledKeyEventListenerCompat = ViewCompat.OnUnhandledKeyEventListenerCompat { v, event ->
+        if (event.keyCode == KeyEvent.KEYCODE_Z && event.isCtrlPressed) {
+            Toast.makeText(
+                v.context,
+                "Undo (Ctrl + Z) shortcut triggered",
+                Toast.LENGTH_LONG
+            ).show()
+            true
+        } else if (event.keyCode == KeyEvent.KEYCODE_F && event.isCtrlPressed) {
+            Toast.makeText(
+                v.context,
+                "Find (Ctrl + F) shortcut triggered",
+                Toast.LENGTH_LONG
+            ).show()
+            true
+        }
+        false
+    }
+
+    private var _binding: FragmentItemListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.addOnUnhandledKeyEventListener(view, unhandledKeyEventListenerCompat)
+
+        val recyclerView: RecyclerView = binding.itemList
+
+        // Leaving this not using view binding as it relies on if the view is visible the current
+        // layout configuration (layout, layout-sw600dp)
+        val itemDetailFragmentContainer: View? = view.findViewById(R.id.item_detail_nav_container)
+
+        setupRecyclerView(recyclerView, itemDetailFragmentContainer)
+    }
+
+    private fun setupRecyclerView(
+        recyclerView: RecyclerView,
+        itemDetailFragmentContainer: View?
+    ) {
+
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(
+            PlaceholderContent.ITEMS, itemDetailFragmentContainer
+        )
+    }
+
+    class SimpleItemRecyclerViewAdapter(
+        private val values: List<PlaceholderContent.PlaceholderItem>,
+        private val itemDetailFragmentContainer: View?
+    ) :
+        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+            val binding = ItemListContentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ViewHolder(binding)
+
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = values[position]
+            holder.idView.text = item.id
+            holder.contentView.text = item.content
+
+            with(holder.itemView) {
+                tag = item
+                setOnClickListener { itemView ->
+                    val item = itemView.tag as PlaceholderContent.PlaceholderItem
+                    val bundle = Bundle()
+                    bundle.putString(
+                        ItemDetailFragment.ARG_ITEM_ID,
+                        item.id
+                    )
+                    if (itemDetailFragmentContainer != null) {
+                        itemDetailFragmentContainer.findNavController()
+                            .navigate(R.id.fragment_item_detail, bundle)
+                    } else {
+                        itemView.findNavController().navigate(R.id.show_item_detail, bundle)
+                    }
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    /**
+                     * Context click listener to handle Right click events
+                     * from mice and trackpad input to provide a more native
+                     * experience on larger screen devices
+                     */
+                    setOnContextClickListener { v ->
+                        val item = v.tag as PlaceholderContent.PlaceholderItem
+                        Toast.makeText(
+                            v.context,
+                            "Context click of item " + item.id,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        true
+                    }
+                }
+
+                setOnLongClickListener { v ->
+                    // Setting the item id as the clip data so that the drop target is able to
+                    // identify the id of the content
+                    val clipItem = ClipData.Item(item.id)
+                    val dragData = ClipData(
+                        v.tag as? CharSequence,
+                        arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                        clipItem
+                    )
+
+                    if (Build.VERSION.SDK_INT >= 24) {
+                        v.startDragAndDrop(
+                            dragData,
+                            View.DragShadowBuilder(v),
+                            null,
+                            0
+                        )
+                    } else {
+                        v.startDrag(
+                            dragData,
+                            View.DragShadowBuilder(v),
+                            null,
+                            0
+                        )
+                    }
+                }
+            }
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(binding: ItemListContentBinding) : RecyclerView.ViewHolder(binding.root) {
+            val idView: TextView = binding.idText
+            val contentView: TextView = binding.content
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

app/src/main/java/com/mnw/deverestinterview/placeholder/PlaceholderContent.kt
@@ -1,0 +1,57 @@
+package com.mnw.deverestinterview.placeholder
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+object PlaceholderContent {
+
+    /**
+     * An array of sample (placeholder) items.
+     */
+    val ITEMS: MutableList<PlaceholderItem> = ArrayList()
+
+    /**
+     * A map of sample (placeholder) items, by ID.
+     */
+    val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
+
+    private val COUNT = 25
+
+    init {
+        // Add some sample items.
+        for (i in 1..COUNT) {
+            addItem(createPlaceholderItem(i))
+        }
+    }
+
+    private fun addItem(item: PlaceholderItem) {
+        ITEMS.add(item)
+        ITEM_MAP.put(item.id, item)
+    }
+
+    private fun createPlaceholderItem(position: Int): PlaceholderItem {
+        return PlaceholderItem(position.toString(), "Item " + position, makeDetails(position))
+    }
+
+    private fun makeDetails(position: Int): String {
+        val builder = StringBuilder()
+        builder.append("Details about Item: ").append(position)
+        for (i in 0..position - 1) {
+            builder.append("\nMore details information here.")
+        }
+        return builder.toString()
+    }
+
+    /**
+     * A placeholder item representing a piece of content.
+     */
+    data class PlaceholderItem(val id: String, val content: String, val details: String) {
+        override fun toString(): String = content
+    }
+}

app/src/test/java/com/mnw/deverestinterview/ExampleUnitTest.kt
@@ -1,0 +1,17 @@
+package com.mnw.deverestinterview
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

mikinw/sampleFlatRock

app/src/androidTest/java/com/mnw/androidinterview/ExampleInstrumentedTest.kt
@@ -1,0 +1,24 @@
+package com.mnw.androidinterview
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.mnw.androidinterview", appContext.packageName)
+    }
+}

app/src/main/java/com/mnw/androidinterview/ItemDetailFragment.kt
@@ -1,0 +1,98 @@
+package com.mnw.androidinterview
+
+import android.content.ClipData
+import android.os.Bundle
+import android.view.DragEvent
+import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.mnw.androidinterview.placeholder.PlaceholderContent
+import com.mnw.androidinterview.databinding.FragmentItemDetailBinding
+
+/**
+ * A fragment representing a single Item detail screen.
+ * This fragment is either contained in a [ItemListFragment]
+ * in two-pane mode (on larger screen devices) or self-contained
+ * on handsets.
+ */
+class ItemDetailFragment : Fragment() {
+
+    /**
+     * The placeholder content this fragment is presenting.
+     */
+    private var item: PlaceholderContent.PlaceholderItem? = null
+
+    lateinit var itemDetailTextView: TextView
+    private var toolbarLayout: CollapsingToolbarLayout? = null
+
+    private var _binding: FragmentItemDetailBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val dragListener = View.OnDragListener { v, event ->
+        if (event.action == DragEvent.ACTION_DROP) {
+            val clipDataItem: ClipData.Item = event.clipData.getItemAt(0)
+            val dragData = clipDataItem.text
+            item = PlaceholderContent.ITEM_MAP[dragData]
+            updateContent()
+        }
+        true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                // Load the placeholder content specified by the fragment
+                // arguments. In a real-world scenario, use a Loader
+                // to load content from a content provider.
+                item = PlaceholderContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemDetailBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+
+        toolbarLayout = binding.toolbarLayout
+        itemDetailTextView = binding.itemDetail
+
+        updateContent()
+        rootView.setOnDragListener(dragListener)
+
+        return rootView
+    }
+
+    private fun updateContent() {
+        toolbarLayout?.title = item?.content
+
+        // Show the placeholder content as text in a TextView.
+        item?.let {
+            itemDetailTextView.text = it.details
+        }
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

app/src/main/java/com/mnw/androidinterview/ItemDetailHostActivity.kt
@@ -1,0 +1,35 @@
+package com.mnw.androidinterview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.mnw.androidinterview.databinding.ActivityItemDetailBinding
+
+
+
+class ItemDetailHostActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityItemDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_item_detail) as NavHostFragment
+        val navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_item_detail)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}

app/src/main/java/com/mnw/androidinterview/ItemListFragment.kt
@@ -1,0 +1,190 @@
+package com.mnw.androidinterview
+
+import android.content.ClipData
+import android.content.ClipDescription
+import android.os.Build
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.mnw.androidinterview.placeholder.PlaceholderContent;
+import com.mnw.androidinterview.databinding.FragmentItemListBinding
+import com.mnw.androidinterview.databinding.ItemListContentBinding
+
+/**
+ * A Fragment representing a list of Pings. This fragment
+ * has different presentations for handset and larger screen devices. On
+ * handsets, the fragment presents a list of items, which when touched,
+ * lead to a {@link ItemDetailFragment} representing
+ * item details. On larger screens, the Navigation controller presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+
+class ItemListFragment : Fragment() {
+
+    /**
+     * Method to intercept global key events in the
+     * item list fragment to trigger keyboard shortcuts
+     * Currently provides a toast when Ctrl + Z and Ctrl + F
+     * are triggered
+     */
+    private val unhandledKeyEventListenerCompat = ViewCompat.OnUnhandledKeyEventListenerCompat { v, event ->
+        if (event.keyCode == KeyEvent.KEYCODE_Z && event.isCtrlPressed) {
+            Toast.makeText(
+                v.context,
+                "Undo (Ctrl + Z) shortcut triggered",
+                Toast.LENGTH_LONG
+            ).show()
+            true
+        } else if (event.keyCode == KeyEvent.KEYCODE_F && event.isCtrlPressed) {
+            Toast.makeText(
+                v.context,
+                "Find (Ctrl + F) shortcut triggered",
+                Toast.LENGTH_LONG
+            ).show()
+            true
+        }
+        false
+    }
+
+    private var _binding: FragmentItemListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentItemListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        ViewCompat.addOnUnhandledKeyEventListener(view, unhandledKeyEventListenerCompat)
+
+        val recyclerView: RecyclerView = binding.itemList
+
+        // Leaving this not using view binding as it relies on if the view is visible the current
+        // layout configuration (layout, layout-sw600dp)
+        val itemDetailFragmentContainer: View? = view.findViewById(R.id.item_detail_nav_container)
+
+        setupRecyclerView(recyclerView, itemDetailFragmentContainer)
+    }
+
+    private fun setupRecyclerView(
+        recyclerView: RecyclerView,
+        itemDetailFragmentContainer: View?
+    ) {
+
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(
+            PlaceholderContent.ITEMS, itemDetailFragmentContainer
+        )
+    }
+
+    class SimpleItemRecyclerViewAdapter(
+        private val values: List<PlaceholderContent.PlaceholderItem>,
+        private val itemDetailFragmentContainer: View?
+    ) :
+        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+            val binding = ItemListContentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return ViewHolder(binding)
+
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = values[position]
+            holder.idView.text = item.id
+            holder.contentView.text = item.content
+
+            with(holder.itemView) {
+                tag = item
+                setOnClickListener { itemView ->
+                    val item = itemView.tag as PlaceholderContent.PlaceholderItem
+                    val bundle = Bundle()
+                    bundle.putString(
+                        ItemDetailFragment.ARG_ITEM_ID,
+                        item.id
+                    )
+                    if (itemDetailFragmentContainer != null) {
+                        itemDetailFragmentContainer.findNavController()
+                            .navigate(R.id.fragment_item_detail, bundle)
+                    } else {
+                        itemView.findNavController().navigate(R.id.show_item_detail, bundle)
+                    }
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    /**
+                     * Context click listener to handle Right click events
+                     * from mice and trackpad input to provide a more native
+                     * experience on larger screen devices
+                     */
+                    setOnContextClickListener { v ->
+                        val item = v.tag as PlaceholderContent.PlaceholderItem
+                        Toast.makeText(
+                            v.context,
+                            "Context click of item " + item.id,
+                            Toast.LENGTH_LONG
+                        ).show()
+                        true
+                    }
+                }
+
+                setOnLongClickListener { v ->
+                    // Setting the item id as the clip data so that the drop target is able to
+                    // identify the id of the content
+                    val clipItem = ClipData.Item(item.id)
+                    val dragData = ClipData(
+                        v.tag as? CharSequence,
+                        arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                        clipItem
+                    )
+
+                    if (Build.VERSION.SDK_INT >= 24) {
+                        v.startDragAndDrop(
+                            dragData,
+                            View.DragShadowBuilder(v),
+                            null,
+                            0
+                        )
+                    } else {
+                        v.startDrag(
+                            dragData,
+                            View.DragShadowBuilder(v),
+                            null,
+                            0
+                        )
+                    }
+                }
+            }
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(binding: ItemListContentBinding) : RecyclerView.ViewHolder(binding.root) {
+            val idView: TextView = binding.idText
+            val contentView: TextView = binding.content
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

app/src/main/java/com/mnw/androidinterview/placeholder/PlaceholderContent.kt
@@ -1,0 +1,57 @@
+package com.mnw.androidinterview.placeholder
+
+import java.util.ArrayList
+import java.util.HashMap
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ *
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+object PlaceholderContent {
+
+    /**
+     * An array of sample (placeholder) items.
+     */
+    val ITEMS: MutableList<PlaceholderItem> = ArrayList()
+
+    /**
+     * A map of sample (placeholder) items, by ID.
+     */
+    val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
+
+    private val COUNT = 25
+
+    init {
+        // Add some sample items.
+        for (i in 1..COUNT) {
+            addItem(createPlaceholderItem(i))
+        }
+    }
+
+    private fun addItem(item: PlaceholderItem) {
+        ITEMS.add(item)
+        ITEM_MAP.put(item.id, item)
+    }
+
+    private fun createPlaceholderItem(position: Int): PlaceholderItem {
+        return PlaceholderItem(position.toString(), "Item " + position, makeDetails(position))
+    }
+
+    private fun makeDetails(position: Int): String {
+        val builder = StringBuilder()
+        builder.append("Details about Item: ").append(position)
+        for (i in 0..position - 1) {
+            builder.append("\nMore details information here.")
+        }
+        return builder.toString()
+    }
+
+    /**
+     * A placeholder item representing a piece of content.
+     */
+    data class PlaceholderItem(val id: String, val content: String, val details: String) {
+        override fun toString(): String = content
+    }
+}

app/src/test/java/com/mnw/androidinterview/ExampleUnitTest.kt
@@ -1,0 +1,17 @@
+package com.mnw.androidinterview
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}